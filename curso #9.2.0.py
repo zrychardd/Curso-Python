479f425d118b5c9e583b638f423b3232bebf4be2
Rychard Eduardo <rychardeduardos@gmail.com>
2025-04-10 21:11:07 -0300

    Delete curso #9.2.0.py

--- curso #9.2.0.py
+++
@@ -1,6 +0,0 @@
-name_city = str(input("Digite o nome da sua City: "))
-
-if name_city.lower().startswith("santo"):
-    print("Sua cidade é Começa com Santo")
-else:
-    print("Sua cidade não Começa com Santo")
